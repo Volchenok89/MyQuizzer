0bcc764c642c7d7349a139ab9bc06950274276aa
Volchenok <warp.speed@hotmail.com>
2021-06-24 21:52:48 -0700

    questions

---
+++ assets/javascript/script.js
@@ -0,0 +1,73 @@
+let testQuestions = [{
+    question: "What is the capital of Iceland?",
+    answerA: "Berlin",
+    answerB: "Ankara",
+    answerC: "Reykjavik",
+    answerD: "Mexico",
+    correctpick: "answerC"
+},
+
+{
+    question: "In coding, what does CSS stands for?",
+    answerA: "Cascade Style Sheet",
+    answerB: "Cannot Say Something",
+    answerC: "Carry Some Sand",
+    answerD: "Canon Style Soft",
+    correctpick: "answerA"
+},
+
+{
+    question: "Which of the following empires had no written language?",
+    answerA: "Egyptian",
+    answerB: "Inca",
+    answerC: "Mayan",
+    answerD: "Roman",
+    correctpick: "answerB"
+},
+
+{
+    question: "Where was the first modern Olympic Games held?",
+    answerA: "Athens",
+    answerB: "Oslo",
+    answerC: "Volgograd",
+    answerD: "Kingston",
+    correctpick: "answerA"
+},
+
+{
+    question: "Which is the longest river in the world?",
+    answerA: "Volga",
+    answerB: "Euphrates",
+    answerC: "Tigris",
+    answerD: "Nile",
+    correctpick: "answerD"
+},
+
+{
+    question: "Disney’s first film?",
+    answerA: "Dumbo",
+    answerB: "Snow White",
+    answerC: "Mulan",
+    answerD: "Aladdin",
+    correctpick: "answerB"
+},
+
+{
+    question: "Name of the Actress featuring on film: Baby Driver?",
+    answerA: "Victoria Beckham",
+    answerB: "Anne Hathaway",
+    answerC: "Eiza Gonzalez",
+    answerD: "Sofia Vergara",
+    correctpick: "answerC"
+},
+
+{
+    question: "Which Honda civic has a double dash?",
+    answerA: "2017 Ex",
+    answerB: "2001 EX-L",
+    answerC: "2012 LX",
+    answerD: "2021 LX",
+    correctpick: "answerC"
+},
+]
+
